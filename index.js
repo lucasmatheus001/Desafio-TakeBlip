127ee6ceadae8e2f268ca385e60111264c6cf6d1
lucasmatheus001 <lucasmatheussm22@gmail.com>
2021-05-24 09:30:08 -0300

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const bodyParser =  require('body-parser')
 const express = require('express')
+const cors  = require('cors')
 const port = process.env.PORT || 4000
 const app = express()
+app.use(cors());
 app.use(express.json())
 
 app.use('/api', require('./src/controllers/TakeDesafios').router)
